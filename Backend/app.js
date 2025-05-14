a3e0d8c5c80888f2f22cdf4486bb910aa0252745
sultan-07 <sultanalih8@gmail.com>
2025-05-14 12:17:38 +0530

    minor

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ connectToDB();
 const app = express();
 
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://uber-clon.netlify.app",
     credentials : true
 }))
 app.use(express.json())
