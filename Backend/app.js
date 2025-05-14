100654e6a3416ada5dcdd6aca4a940a0546c2509
sultan-07 <sultanalih8@gmail.com>
2025-05-14 12:02:40 +0530

    minor

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ connectToDB();
 const app = express();
 
 app.use(cors({
-    origin : "http://http://localhost:5173",
+    origin : "http://localhost:5173",
     credentials : true
 }))
 app.use(express.json())
